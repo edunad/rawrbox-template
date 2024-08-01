63ab393736c4b4065cb519243637251bf2c9fc02
FailCake <edunad@users.noreply.github.com>
2024-08-01 19:59:47 +0200

    - Update CPM - Update game

--- code/src/game.cpp
+++ code/src/game.cpp
@@ -34,7 +34,7 @@ namespace my_game {
 		render->setDrawCall([this](const rawrbox::DrawPass& pass) {
 			if (pass == rawrbox::DrawPass::PASS_OVERLAY) {
 				this->drawOverlay();
-			} else if (pass == rawrbox::DrawPass::PASS_OPAQUE) {
+			} else if (pass == rawrbox::DrawPass::PASS_WORLD) {
 				this->drawWorld();
 			}
 		});
